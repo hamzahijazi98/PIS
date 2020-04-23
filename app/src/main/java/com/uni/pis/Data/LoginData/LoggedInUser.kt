6e243db48ebb5944bfdfc588322ae0f3fb1bc79f
hamzahijazi <hamzahijazi98@gmail.com>
2020-04-23 16:19:35 +0300

    toast fixing

--- app/src/main/java/com/uni/pis/Data/LoginData/LoggedInUser.kt
+++ app/src/main/java/com/uni/pis/Data/LoginData/LoggedInUser.kt
@@ -1,7 +1,7 @@
 package com.uni.pis.Data.LoginData
 
 /**
- * Data class that captures user information for logged in users retrieved from LoginRepository
+ * Data class that captures user information for logged in users retrieved from LoginRepository fjdsjkf
  */
 data class LoggedInUser(
     val displayName: String,
